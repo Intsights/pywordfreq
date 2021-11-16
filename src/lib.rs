e81b272567efd9f614af0a20b687382e6ad863a1
Gal Ben David <gal@intsights.com>
2021-11-16 14:17:50 +0200

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,121 +1,115 @@
+use ahash::{AHashMap, AHashSet};
+use flate2::read::GzDecoder;
+use once_cell::unsync::Lazy;
 use pyo3::prelude::*;
-use std::collections::{HashMap, HashSet};
+use pyo3::types::PyUnicode;
+use std::io::prelude::*;
 use suffix::SuffixTable;
 
 static mut WORDS_TEXT: String = String::new();
-
-#[pyclass]
-#[text_signature = "(word_frequencies, /)"]
-struct WordFrequency {
-    suffix_table: SuffixTable<'static, 'static>,
-    word_frequencies: HashMap<&'static str, usize>,
-}
-
-#[pymethods]
-impl WordFrequency {
-    #[new]
-    fn new(
-        word_frequencies_text: &str,
-        min_frequency: usize,
-    ) -> Self {
-        let mut number_of_words: usize = 0;
-        let mut total_words_text_len: usize = 1;
-        word_frequencies_text
-            .lines()
-            .for_each(
-                |line| {
-                    let splitted_line: Vec<&str> = line.split(';').collect();
-                    let word: &str = splitted_line[0];
-                    let frequency: usize = splitted_line[1].parse().unwrap();
-                    if frequency >= min_frequency {
-                        number_of_words += 1;
-                        total_words_text_len += word.len() + 1;
-                    }
-                }
-            );
-
-        let mut word_frequencies = HashMap::<&'static str, usize>::with_capacity(number_of_words);
-        let suffix_table: SuffixTable;
-        unsafe {
-            WORDS_TEXT.reserve(total_words_text_len);
-            WORDS_TEXT.push('\n');
-
-            word_frequencies_text
-                .lines()
-                .filter(|line| !line.is_empty())
-                .for_each(
-                    |line| {
-                        let splitted_line: Vec<&str> = line.split(';').collect();
-                        let word: &str = splitted_line[0];
-                        let frequency: usize = splitted_line[1].parse().unwrap();
-                        if frequency >= min_frequency {
-                            WORDS_TEXT.push_str(word);
-                            WORDS_TEXT.push('\n');
-                            let end_of_word = WORDS_TEXT.len() - 1;
-                            let start_of_word = WORDS_TEXT.len() - 1 - word.len();
-
-                            word_frequencies.insert(
-                                &WORDS_TEXT[start_of_word..end_of_word],
-                                frequency,
-                            );
-                        }
-                    }
-                );
-
-                suffix_table = SuffixTable::new(WORDS_TEXT.as_str());
-            }
-            WordFrequency { suffix_table, word_frequencies }
-        }
-
-    #[text_signature = "(word, /)"]
-    fn full_frequency(
-        &self,
-        word: &str,
-    ) -> usize {
-        match self.word_frequencies.get(word.to_ascii_lowercase().as_str()) {
-            Some(frequency) => {
-                frequency.to_owned()
-            },
-            None => 0
-        }
+static mut SUFFIX_TABLE: Lazy<SuffixTable<'static, 'static>> = Lazy::new(
+    || unsafe {
+        SuffixTable::new(WORDS_TEXT.as_str())
     }
-
-    #[text_signature = "(pattern, /)"]
-    fn partial_frequency(
-        &self,
-        pattern: &str,
-    ) -> usize {
-        let mut cumulative_frequency = 0usize;
-        let mut found_words = HashSet::<&str>::new();
-
-        let word_lowered = pattern.to_ascii_lowercase();
-
-        self.suffix_table.positions(word_lowered.as_str())
-            .iter()
-            .for_each(
-                |suffix_index| {
-                    let start_index: usize = match &self.suffix_table.text()[..*suffix_index as usize].rfind('\n') {
-                        Some(start_index) => start_index + 1,
-                        None => 0,
-                    };
-                    let end_index: usize = match &self.suffix_table.text()[*suffix_index as usize..].find('\n') {
-                        Some(end_index) => *suffix_index as usize + end_index,
-                        None => self.suffix_table.text().len(),
-                    };
-                    let full_word = &self.suffix_table.text()[start_index..end_index];
-                    if full_word != word_lowered && found_words.insert(full_word) {
-                        cumulative_frequency += *self.word_frequencies.get(full_word).unwrap_or(&0);
-                    }
-                }
-            );
-
-        cumulative_frequency
+);
+static mut WORD_FREQUENCIES: Lazy<AHashMap<&'static str, usize>> = Lazy::new(
+    || {
+        AHashMap::new()
     }
-}
+);
+static mut WORD_FREQUENCIES_BY_START_INDEX: Lazy<AHashMap<usize, usize>> = Lazy::new(
+    || {
+        AHashMap::new()
+    }
+);
+static mut FOUND_WORDS_START_INDEX: Lazy<AHashSet<usize>> = Lazy::new(
+    || {
+        AHashSet::with_capacity(1000)
+    }
+);
+
 
 #[pymodule]
 fn pywordfreq(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<WordFrequency>()?;
+    #[pyfn(m)]
+    fn load_dictionary(
+        dictionary_compressed: &[u8],
+    ) {
+        let mut dictionary_decoder = GzDecoder::new(dictionary_compressed);
+        let mut word_frequencies_text = String::new();
+        dictionary_decoder.read_to_string(&mut word_frequencies_text).unwrap();
+
+        let mut number_of_words = 0;
+        let mut total_words_text_len = 0;
+        for line in word_frequencies_text.lines() {
+            number_of_words += 1;
+            total_words_text_len += line.rfind(';').unwrap() + 1;
+        }
+
+        unsafe {
+            WORD_FREQUENCIES.reserve(number_of_words);
+            WORDS_TEXT.reserve(total_words_text_len);
+
+            for line in word_frequencies_text.lines() {
+                if let Some((word, frequency)) = line.rsplit_once(';') {
+                    let frequency: usize = frequency.parse().unwrap();
+                    let start_of_word = WORDS_TEXT.len();
+                    WORDS_TEXT.push_str(word);
+                    WORDS_TEXT.push('\n');
+
+                    WORD_FREQUENCIES.insert(
+                        WORDS_TEXT.get_unchecked(start_of_word..start_of_word + word.len()),
+                        frequency,
+                    );
+                    WORD_FREQUENCIES_BY_START_INDEX.insert(
+                        start_of_word,
+                        frequency,
+                    );
+                }
+            }
+        }
+    }
+
+    #[pyfn(m)]
+    fn full_frequency(
+        word: &PyUnicode,
+    ) -> usize {
+        unsafe {
+            match WORD_FREQUENCIES.get(word.to_string_lossy().to_ascii_lowercase().as_str()) {
+                Some(frequency) => {
+                    *frequency
+                },
+                None => 0
+            }
+        }
+    }
+
+    #[pyfn(m)]
+    fn partial_frequency(
+        pattern: &str,
+    ) -> usize {
+        let word_lowered = pattern.to_ascii_lowercase();
+        let mut cumulative_frequency = 0usize;
+
+        unsafe {
+            FOUND_WORDS_START_INDEX.clear();
+            let suffix_table_text = SUFFIX_TABLE.text();
+
+            for suffix_index in SUFFIX_TABLE.positions(word_lowered.as_str()) {
+                let start_index: usize = match suffix_table_text.get_unchecked(..*suffix_index as usize).rfind('\n') {
+                    Some(start_index) => start_index + 1,
+                    None => 0,
+                };
+                if FOUND_WORDS_START_INDEX.insert(start_index) {
+                    cumulative_frequency += WORD_FREQUENCIES_BY_START_INDEX.get(&start_index).unwrap_or(&0);
+                }
+            }
+
+            cumulative_frequency -= WORD_FREQUENCIES.get(word_lowered.as_str()).unwrap_or(&0);
+        }
+
+        cumulative_frequency
+    }
 
     Ok(())
 }
