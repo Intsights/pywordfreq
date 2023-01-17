3417ea9d2911e47d9a93894ee79283079e5fa912
michael-lerner <michael_lerner@rapid7.com>
2023-01-17 16:16:43 +0200

    Upgrade to python3.11

--- pywordfreq/__init__.py
+++ pywordfreq/__init__.py
@@ -1,16 +1,32 @@
 import importlib.resources
+import sys
 
 from . import pywordfreq
+
+PY_VERSION_MAJOR = sys.version_info.major
+PY_VERSION_MINOR = sys.version_info.minor
 
 
 def lazy_full_frequency(
     word,
 ):
-    pywordfreq.load_dictionary(
-        importlib.resources.read_binary(
+    if PY_VERSION_MAJOR >= 3 and PY_VERSION_MINOR >= 11:
+        with importlib.resources.files(
+            __package__,
+        ).joinpath(
+            'word_frequencies.gz',
+        ).open(
+            'rb',
+        ) as _word_frequencies_binary:
+            word_frequencies_binary = _word_frequencies_binary
+    else:
+        word_frequencies_binary = importlib.resources.read_binary(
             package=__package__,
             resource='word_frequencies.gz',
         )
+
+    pywordfreq.load_dictionary(
+        word_frequencies_binary
     )
 
     global full_frequency
@@ -19,17 +35,31 @@ def lazy_full_frequency(
     full_frequency = pywordfreq.full_frequency
     partial_frequency = pywordfreq.partial_frequency
 
-    return pywordfreq.full_frequency(word)
+    return pywordfreq.full_frequency(
+        word
+    )
 
 
 def lazy_partial_frequency(
     pattern,
 ):
-    pywordfreq.load_dictionary(
-        importlib.resources.read_binary(
+    if PY_VERSION_MAJOR >= 3 and PY_VERSION_MINOR >= 11:
+        with importlib.resources.files(
+            __package__,
+        ).joinpath(
+            'word_frequencies.gz',
+        ).open(
+            'rb',
+        ) as _word_frequencies_binary:
+            word_frequencies_binary = _word_frequencies_binary.read()
+    else:
+        word_frequencies_binary = importlib.resources.read_binary(
             package=__package__,
             resource='word_frequencies.gz',
         )
+
+    pywordfreq.load_dictionary(
+        word_frequencies_binary
     )
 
     global full_frequency
@@ -38,7 +68,9 @@ def lazy_partial_frequency(
     full_frequency = pywordfreq.full_frequency
     partial_frequency = pywordfreq.partial_frequency
 
-    return pywordfreq.partial_frequency(pattern)
+    return pywordfreq.partial_frequency(
+        pattern
+    )
 
 
 full_frequency = lazy_full_frequency
